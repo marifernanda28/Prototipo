4443ec31c9144d96649fb5aa327a5477c3f972da
marifernanda28 <132498100+marifernanda28@users.noreply.github.com>
2025-05-27 15:35:37 -0300

    site

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 <script src="main.js"></script>
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 console.log(botoes);
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll
 for (let i = 0; i < botoes.length; i++) {
   console.log(i);
 }

a8add4a8ea17a283b3d4cd897af932f39d7316fd
marifernanda28 <132498100+marifernanda28@users.noreply.github.com>
2025-05-29 08:52:12 -0300

    prototipo

--- main.js
+++ main.js
@@ -11,3 +11,37 @@ botoes[i].onclick = function () {};
 botoes[i].onclick = function () {
   botoes[i].classList.add("ativo");
 };
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
